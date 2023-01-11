c135a7752192ae82a9288d5c0aeb4795de6b6232
Bifrost <103068176+bifrostyyy@users.noreply.github.com>
2023-01-11 10:27:56 -0500

    Update delete_auth_emulator_users.sh

--- scripts/delete_auth_emulator_users.sh
+++ scripts/delete_auth_emulator_users.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 curl -H 'Authorization: Bearer owner' -X DELETE \
-	'http://localhost:9099/emulator/v1/projects/bionic-water-366401/accounts'
+	'http://localhost:9099/emulator/v1/projects/dev-verifi/accounts'
